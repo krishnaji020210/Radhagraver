96d2866eb30a2af8107c729fd0791ff930d5e832
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-07-31 16:35:50 +0530

    Add files via upload

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -1,1 +1,42 @@
+import asyncio
+import logging
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+
+
+loop = asyncio.get_event_loop()
+
+app = Client(
+    ":MusicBot:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+userbot = Client(
+    ":userbot:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION_STRING,
+)
+
+pytgcalls = PyTgCalls(userbot)
+
+async def Info_Music():
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    await app.start()
+    await userbot.start()
+    await pytgcalls.start()
+    getme = await app.get_me()
+    BOT_ID = getme.id
+    BOT_USERNAME = getme.username
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+
+
+loop.run_until_complete(Info_Music())
+
 
