a9093b74266d8eddd4209608e2da7d166a058b99
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-08-02 11:36:21 +0530

    Update waifuchat.py

--- Grabber/modules/waifuchat.py
+++ Grabber/modules/waifuchat.py
@@ -1,19 +1,39 @@
-from pyrogram import filters 
+from pyrogram import filters
 from Grabber import app
 from Grabber.core import main_func
 from Grabber.core.brain import instructions
 
 
-
 @app.on_message(filters.command("chatwaifu"))
 async def waifu_chat(_, message):
     user_id = message.chat.id
-    name = message.from_user.first_name
+    name = message.from_user.first_name or "Anon"
 
-    if len(message.command) < 2:
-        return await message.reply_text("hey baby if you want talk to me then give this /chatwaifu query")
-      
-    query = message.text.split(maxsplit=1)[1]
+    if not message.text or len(message.command) < 2:
+        return await message.reply_text(
+            "Hey baby  If you want to talk to me, use:\n\n`/chatwaifu your message here`"
+        )
+
+    try:
+        query = message.text.split(None, 1)[1]
+    except IndexError:
+        return await message.reply_text(
+            "Hmm? You forgot to add what you wanted to say!\n\nTry again like:\n`/chatwaifu Hi, how are you?`"
+        )
+
     char_prompt = await instructions.generate_char(user_id, "Hinata", name)
     answer = await main_func.gemini_response(query, char_prompt)
     await instructions.chat_conversation(user_id, query, answer)
+    await message.reply_text(answer)
+
+
+
+
+@app.on_message(filters.command("clear"))
+async def delete_chathistory(_, message):
+    user_id = message.from_user.id
+    database = await instructions.delete_data(user_id)
+    if database.deleted_count:
+        await message.reply_text(f"{message.from_user.first_name} your chat history database deleted successfully.")
+    else:
+        await message.reply_text(f"{message.from_user.first_name} not save any database!!")
