e6a04f199df660468ec59bb810a8fc40994a3f28
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-08-02 09:50:41 +0530

    Create main_func.py

---
+++ Grabber/core/main_func.py
@@ -0,0 +1,37 @@
+import requests 
+
+
+
+# --------------------------- Gemini-Response --------------------------- #         
+
+gemini_key = "AIzaSyB1rjWN0smsStTaakoSNaz9JveYTtDkXPY"
+
+async def gemini_response(query, prompt):
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={gemini_key}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-goog-api-key": gemini_key
+    }
+
+    data = {
+        "contents": [{
+            "parts": [{"text": f"{prompt}: {query}"}]
+        }]
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            res_json = response.json()
+            return res_json["candidates"][0]["content"]["parts"][0]["text"]
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+            return "Bot API Dead!! Please Contact My Owner."
+
+    except Exception as e:
+        print(f"Exception: {e}")
+        return "Bot API Dead!! Please Contact My Owner."
+
+
+
