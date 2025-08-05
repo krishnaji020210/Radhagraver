9006e79e0b464021cf0d037ccda550ff7b288d03
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-08-06 00:10:25 +0530

    Update mywaifus.py

--- Grabber/modules/mywaifus.py
+++ Grabber/modules/mywaifus.py
@@ -1,5 +1,6 @@
 from Grabber import app
 from pyrogram import filters
+from Grabber.core import main_func
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
 from Grabber.core.mongo.waifusdb import getUserAllWaifus
 
@@ -10,14 +11,14 @@ def format_waifus_list(waifus, page=0, per_page=5):
     end = start + per_page
     page_waifus = waifus[start:end]
 
-    text = f"🧾️ <b>Waifu Grab</b> - ({min(end, total)}/{total})\n━━━━━━━━━━━━━━━━━━━━━━━━\n"
+    text = f"🏵 <b>Waifu Grab</b> - ({min(end, total)}/{total})\n━━━━━━━━━━━━━━━━━━\n"
     for w in page_waifus:
         text += (
             f"📑 <b>ID</b>: <code>{w['waifu_id']}</code>\n"
             f"🧽️ <b>Name</b>: {w['name']}\n"
             f"🧩 <b>Anime</b>: {w['anime']} - {w['grab_count']}x\n"
             f"🎭 <b>Rarity</b>: {w['rank']}\n"
-            f"┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n"
+            f"┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n"
         )
     return text
 
@@ -25,10 +26,10 @@ def get_buttons(page, total, per_page=5):
     max_pages = (total - 1) // per_page
     buttons = []
     if page > 0:
-        buttons.append(InlineKeyboardButton("⬅️ Prev", callback_data=f"waifus_prev_{page-1}"))
+        buttons.append(InlineKeyboardButton("﹤ Prev", callback_data=f"waifus_prev_{page-1}"))
     if page < max_pages:
-        buttons.append(InlineKeyboardButton("Next ➡️", callback_data=f"waifus_next_{page+1}"))
-    buttons.append(InlineKeyboardButton("✖️ Close", callback_data="waifus_close"))
+        buttons.append(InlineKeyboardButton("Next ﹥", callback_data=f"waifus_next_{page+1}"))
+    buttons.append(InlineKeyboardButton("☌ Close", callback_data="waifus_close"))
     return InlineKeyboardMarkup([buttons])
 
 @app.on_message(filters.command("mywaifus"))
@@ -44,6 +45,9 @@ async def mywaifus_handler(client, message):
     top_image = waifus[0]["image"]
 
     await message.reply_photo(photo=top_image, caption=text, reply_markup=buttons)
+
+
+
 
 @app.on_callback_query(filters.regex(r"waifus_(next|prev)_(\d+)"))
 async def paginate_waifus(client, callback_query):
