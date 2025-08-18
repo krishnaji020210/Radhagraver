5c4d713403f1b74d86f1e618d67d1ab67f8bf625
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-08-18 23:20:11 +0530

    Update settingsdb.py

--- Grabber/core/mongo/settingsdb.py
+++ Grabber/core/mongo/settingsdb.py
@@ -4,7 +4,9 @@ from config import MONGO_DB
 mongo = MongoCli(MONGO_DB)
 db = mongo.settingsdb
 time_collection = db.spwantime_db  
+coins_collection = db.users_coins
 
+# ----------------------- Get Spawn Time ----------------------- #
 
 async def change_spawn_time(chat_id: int, count: int = 100) -> None:
     await time_collection.update_one(
@@ -14,6 +16,27 @@ async def change_spawn_time(chat_id: int, count: int = 100) -> None:
     )
 
 
+# ----------------------- Change Spawn Time ----------------------- #
+
 async def get_spawn_time(chat_id: int) -> int:
     result = await time_collection.find_one({"_id": chat_id})
     return result["count"] if result else 100
+
+
+
+# ----------------------- Add Coins ----------------------- #
+
+async def add_coins(user_id, coins):
+    await coins_collection.update_one(
+        {"_id": user_id},
+        {"$set": {"coins": coins}},
+        upsert=True
+    )
+
+
+# ----------------------- Get Coins ----------------------- #
+
+async def get_coins(user_id: int) -> int:
+    result = await coins_collection.find_one({"_id": user_id})
+    return result["count"] if result else 0
+
