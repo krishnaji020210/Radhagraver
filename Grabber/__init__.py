6436500ae3d0c33ff8ced5c418be5c59440fa972
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-02-19 13:16:44 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -1,8 +1,6 @@
 import asyncio
 import os
-import uvicorn
 from pyromod import listen
-from fastapi import FastAPI
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
@@ -11,9 +9,7 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 loop = asyncio.get_event_loop()
 
-
 # ----------------------------- App-Client ----------------------------- #
-api = FastAPI()
 app = Client(":WaifuGrabber:", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 BOT_ID = BOT_NAME = BOT_USERNAME = None
@@ -28,15 +24,6 @@ async def start_app():
     BOT_NAME = f"{getme.first_name} {getme.last_name}" if getme.last_name else getme.first_name
     print(f"Bot started as @{BOT_USERNAME}")
 
-async def start_server():
-    port = int(os.getenv("PORT", 8000))
-    config = uvicorn.Config(api, host="0.0.0.0", port=port, workers=1)
-    server = uvicorn.Server(config)
-    await server.serve()
-
-
-
-loop.create_task(start_server())
 loop.run_until_complete(start_app())
 
 # ---------------------------------------------------------------- #
