9f216aaa8e76dd1f64dc7a471de37691b989d8a3
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-02-21 13:33:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "8066896805:AAFH6UekFrbDcdhAYbYV71mBepTQ-GtAgOk")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8462359928").split()))
-SUDO_ID = list(map(int, getenv("SUDO_ID", "8462359928").split()))
+SUDO_IDS = list(map(int, getenv("SUDO_IDS", "8462359928").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://uploader:g3UouNkbo1kZXCEL@cluster0.wzsxgoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/DevsLaboratory")
