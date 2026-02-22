ed741eaa82a7386fa8d67477013cb7268ced1d66
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-02-22 19:18:53 +0530

    Update settingsdb.py

--- Grabber/core/mongo/settingsdb.py
+++ Grabber/core/mongo/settingsdb.py
@@ -1,7 +1,8 @@
 from Grabber.core.mongo import database
 
-# --------------------------- Spawn Collection --------------------------- #
+# --------------------------- Core Collection --------------------------- #
 time_collection = database.spwantime_db
+married_collection = database.marrried_db
 # --------------------------- Core --------------------------- #
 
 
@@ -19,4 +20,18 @@ async def get_spawn_time(chat_id: int) -> int:
     result = await time_collection.find_one({"_id": chat_id})
     return result.get("count", 100) if result else 100
 
+
+# ----------------------- Add Married Data ----------------------- #
+async def add_married(user_id: int, name: str = "natasha", divorce: str = False) -> None:
+    await married_collection.update_one(
+        {"_id": user_id},
+        {"$set": {"name": name, "divorce", divorce}},
+        upsert=True
+    )
+
+
+# ----------------------- Get Married Data ----------------------- #
+async def get_married(user_id: int) -> int:
+    result = await married_collection.find_one({"_id": user_id})
+    return result.get("name", "natasha") if result else "natasha"
 
