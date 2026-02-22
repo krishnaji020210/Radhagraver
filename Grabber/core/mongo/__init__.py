aaf926b307f2b1b956e55c2d6c2b3f7b82b830e5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-02-22 09:34:40 +0530

    Update __init__.py

--- Grabber/core/mongo/__init__.py
+++ Grabber/core/mongo/__init__.py
@@ -1,1 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB
 
+# --------------------------- MongoDB Client --------------------------- #
+
+mongo = AsyncIOMotorClient(MONGO_DB)
+database = mongo.waifu_grabbers
